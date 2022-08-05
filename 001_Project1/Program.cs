0e9c0bb5d4486e12fd3133e58215ffb88c80ad41
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-05 23:37:46 +0500

    if else added

--- 001_Project1/Program.cs
+++ 001_Project1/Program.cs
@@ -1,6 +1,36 @@
-﻿Console.WriteLine("Print first number: ");
-int number1 = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine("Print second numder: ");
-int number2 = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine("Summa:");
-Console.WriteLine (number1 + number2);
+﻿while (true)
+{
+    Console.WriteLine("Print sing or end to exit: ");
+    string Sing = Console.ReadLine();
+    if (Sing == "end")
+    {
+        break;
+    }
+    Console.WriteLine("Print first number: ");
+    double number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Print second numder: ");
+    double number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Print sing: ");
+    if (Sing == "+")
+    {
+        Console.Write("Result: ");
+        Console.WriteLine(number1 + number2);
+    }
+    else if (Sing == "-")
+    {
+        Console.Write("Result: ");
+        Console.WriteLine(number1 - number2);
+    }
+    else if (Sing == "*")
+    {
+        Console.Write("Result: ");
+        Console.WriteLine(number1 * number2);
+    }
+    else if (Sing == "/")
+    {
+        Console.Write("Result: ");
+        Console.WriteLine(number1 / number2);
+    }
+
+}
+
