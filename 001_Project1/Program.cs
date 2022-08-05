b4eb0450fabd5f5834b841c23eadb0d91a293124
Popova Ekaterina <popova_katerina97@mail.ru>
2022-08-05 22:49:26 +0500

    Find a sum

--- 001_Project1/Program.cs
+++ 001_Project1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Print first number: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Print second numder: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Summa:");
+Console.WriteLine (number1 + number2);
